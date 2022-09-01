7dc8085d89818d8668218ba8b658a62cb2293f93
Mykhailo Lopaiev <mykhailo_lopaiev@epam.com>
2022-09-01 11:30:28 +0300

    added re-init for default gateways and removed hardcoded value for the dhcp interface

--- ispmon.sh
+++ ispmon.sh
@@ -6,10 +6,10 @@ if [[ $# -eq 0 ]]; then
 fi
 
 # Define ISP gateways
-gw_1=255.255.255.255
 iface_1=eth0
-gw_2=$(/sbin/ip route show | grep "ppp0 proto" | cut -d ' ' -f1)
 iface_2=ppp0
+gw_1=$(grep </var/lib/dhcp/dhclient."$iface_1".leases routers | tail -n 1 | grep -oP '(\d+\.){3}\d+')
+gw_2=$(/sbin/ip route show | grep "ppp0 proto" | cut -d ' ' -f1)
 
 company=BANANA
 
@@ -18,43 +18,49 @@ Help() {
 }
 
 # Get the options
-while getopts "hstane" option; do
-    case $option in
-    h) # display Help
-        Help
-        exit
-        ;;
-    s) # Set primary gateway to Sana
-        primary_gw=$gw_1
-        secondary_gw=$gw_2
-        primary_iface=$iface_1
-        secondary_iface=$iface_2
-        primary_isp_name=Sana
-        secondary_isp_name=TeNet
-        ;;
-    t) # Set primary gateway to TeNet
-        primary_gw=$gw_2
-        secondary_gw=$gw_1
-        primary_iface=$iface_2
-        secondary_iface=$iface_1
-        primary_isp_name=TeNet
-        secondary_isp_name=Sana
-        ;;
-    a) # systemd service hack
-        /bin/true
-        ;;
-    n) # systemd service hack
-        /bin/true
-        ;;
-    e) # systemd service hack
-        /bin/true
-        ;;
-    \?)
-        echo "Invalid option"
-        exit 1
-        ;;
-    esac
-done
+
+define_gateways() {
+    local OPTIND
+    while getopts "hstane" option; do
+        case $option in
+        h) # display Help
+            Help
+            exit
+            ;;
+        s) # Set primary gateway to Sana
+            primary_gw=$gw_1
+            secondary_gw=$gw_2
+            primary_iface=$iface_1
+            secondary_iface=$iface_2
+            primary_isp_name=Sana
+            secondary_isp_name=TeNet
+            ;;
+        t) # Set primary gateway to TeNet
+            primary_gw=$gw_2
+            secondary_gw=$gw_1
+            primary_iface=$iface_2
+            secondary_iface=$iface_1
+            primary_isp_name=TeNet
+            secondary_isp_name=Sana
+            ;;
+        a) # systemd service hack
+            /bin/true
+            ;;
+        n) # systemd service hack
+            /bin/true
+            ;;
+        e) # systemd service hack
+            /bin/true
+            ;;
+        \?)
+            echo "Invalid option"
+            exit 1
+            ;;
+        esac
+    done
+}
+
+define_gateways "$@"
 
 sendstatus() {
     echo "$1" | mail -s "$2" test@example.com
@@ -66,16 +72,17 @@ add_nameservers() {
 }
 
 # Clean up dangling route in case script is killed or fails
-testhost=8.8.8.8
+testhost=1.1.1.1
 if [ "$(/sbin/ip route show | grep -c $testhost)" != 0 ]; then
     /sbin/ip route del $testhost
 fi
 
 # Switch default route to primary ISP immediately upon call so we don't have to wait
 if /sbin/ip route add $testhost via "$primary_gw" dev $primary_iface; then
-    if ping $testhost -c 1 -q -W 1; then
+    if ping $testhost -c 1 -q -W 1 > /dev/null; then
         /sbin/ip route delete default && /sbin/ip route add default via "$primary_gw" dev $primary_iface
         add_nameservers
+        echo "Switched to the defined primary gateway"
     fi
     /sbin/ip route del $testhost via "$primary_gw" dev $primary_iface
 fi
@@ -127,5 +134,12 @@ while true; do
 
     # Good night
     sleep 120
+
+    # Re-define gateways in case they change
+
+    gw_1=$(grep </var/lib/dhcp/dhclient."$iface_1".leases routers | tail -n 1 | grep -oP '(\d+\.){3}\d+')
+    gw_2=$(/sbin/ip route show | grep "ppp0 proto" | cut -d ' ' -f1)
+
+    define_gateways "$@"
 
 done
